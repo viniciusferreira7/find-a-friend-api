44ce3eeab4f89bf2411e9c76a354835192cad3dc
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-07-30 08:52:24 -0300

    feat: creates the prisma repository for pet tables

---
+++ src/repositories/pets/prisma-pets-repository.ts
@@ -0,0 +1,28 @@
+import { Pet, Prisma } from '@prisma/client'
+
+import { prisma } from '../../lib/prisma'
+import { PetsRepository } from '../pets-repository'
+
+export class PrismaPetsRepository implements PetsRepository {
+  async create({
+    organizationId,
+    ...pet
+  }: Prisma.PetUncheckedCreateInput): Promise<Pet> {
+    const createdPet = await prisma.pet.create({
+      data: {
+        organizationId,
+        name: pet.name,
+        description: pet.description,
+        petAge: pet.petAge,
+        petSize: pet.petSize,
+        petSpecies: pet.petSpecies,
+        petEnergyLevel: pet.petEnergyLevel,
+        petIndependenceLevel: pet.petIndependenceLevel,
+        petSuitableEnvironment: pet.petSuitableEnvironment,
+        petImageUrl: pet.petImageUrl,
+      },
+    })
+
+    return createdPet
+  }
+}
