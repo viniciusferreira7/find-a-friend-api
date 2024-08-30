a78806bff5005d2035d5411221c47c8612ecdc5b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-08-30 08:43:42 -0300

    feat creates the authentication use case

---
+++ prisma/migrations/20240830112916_removes_users_table/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "users" DROP CONSTRAINT "users_petId_fkey";
+
+-- DropTable
+DROP TABLE "users";
