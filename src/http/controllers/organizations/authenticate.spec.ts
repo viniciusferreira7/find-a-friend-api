071b2c2cd251cc5fed90c352497e15fd438b4f1a
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-09-03 08:46:56 -0300

    test: creates test E2E to authenticate route

---
+++ src/http/controllers/organizations/authenticate.spec.ts
@@ -0,0 +1,42 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
+
+describe('Authenticate (E2E', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    const email = 'john.doe@example.org'
+    const password = '123456'
+
+    await request(app.server).post('/organizations').send({
+      managerName: 'John Doe',
+      email,
+      password,
+      cep: '12345678',
+      street: 'Elm Street',
+      number: 1234,
+      complement: 'Suite 5B',
+      city: 'Springfield',
+      state: 'IL',
+      cellPhoneNumber: '12345678910',
+    })
+
+    const response = await request(app.server)
+      .post('/organizations/sessions')
+      .send({
+        email,
+        password,
+      })
+
+    expect(response.status).toBe(200)
+    expect(response.body.token).toEqual(expect.any(String))
+  })
+})
