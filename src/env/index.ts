f552e91b665fc3626974954c722efe9e31591cb1
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-09-10 08:30:17 -0300

    build: adjusts the env for the CI environment

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,67 +1,15 @@
 import { z } from 'zod'
 
-export const envSchema = z
-  .object({
-    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev').optional(),
-    PORT: z.coerce.number().default(3333).optional(),
-    DATABASE_USERNAME: z.string().optional(),
-    DATABASE_PASSWORD: z.string().optional(),
-    DATABASE_NAME: z.string().optional(),
-    DATABASE_URL: z.string().url().optional(),
-    JWT_SECRET: z.string(),
-    APP_TOKEN: z.string(),
-  })
-  .superRefine((data, ctx) => {
-    if (process.env.CI === 'true') {
-      return
-    }
-
-    if (!data.DATABASE_USERNAME) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'DATABASE_USERNAME is required',
-        path: ['DATABASE_USERNAME'],
-      })
-    }
-
-    if (!data.DATABASE_PASSWORD) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'DATABASE_PASSWORD is required',
-        path: ['DATABASE_PASSWORD'],
-      })
-    }
-
-    if (!data.DATABASE_NAME) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'DATABASE_NAME is required',
-        path: ['DATABASE_NAME'],
-      })
-    }
-
-    if (!data.DATABASE_URL) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'DATABASE_URL is required',
-        path: ['DATABASE_URL'],
-      })
-    }
-    if (!data.JWT_SECRET) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'DATABASE_URL is required',
-        path: ['DATABASE_URL'],
-      })
-    }
-    if (!data.APP_TOKEN) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'DATABASE_URL is required',
-        path: ['DATABASE_URL'],
-      })
-    }
-  })
+export const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+  DATABASE_USERNAME: z.string(),
+  DATABASE_PASSWORD: z.string(),
+  DATABASE_NAME: z.string(),
+  DATABASE_URL: z.string().url(),
+  JWT_SECRET: z.string(),
+  APP_TOKEN: z.string(),
+})
 
 const _env = envSchema.safeParse(process.env)
 
