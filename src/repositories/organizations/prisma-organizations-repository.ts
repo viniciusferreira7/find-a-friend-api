983e4700d588b632ffbcdc8177ad6ea5b5fb6c3b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-09-24 07:50:55 -0300

    feat: adds method to find many pets by city of organization in prisma repository

--- src/repositories/organizations/prisma-organizations-repository.ts
+++ src/repositories/organizations/prisma-organizations-repository.ts
@@ -1,10 +1,123 @@
-import { Organization, Prisma } from '@prisma/client'
+import { Organization, type Pet, Prisma } from '@prisma/client'
 
+import type {
+  PaginationRequest,
+  PaginationResponse,
+} from '@/interfaces/pagination'
+import type {
+  PetAge,
+  PetEnergyLevel,
+  PetIndependenceLevel,
+  PetSize,
+} from '@/interfaces/pets'
 import { prisma } from '@/lib/prisma'
 
 import { OrganizationsRepository } from '../organizations-repository'
 
+interface SearchParams extends PaginationRequest {
+  state: string
+  city: string
+  age?: PetAge
+  energyLevel?: PetEnergyLevel
+  size?: PetSize
+  species?: string
+  independenceLevel?: PetIndependenceLevel
+}
+
 export class PrismaOrganizationsRepository implements OrganizationsRepository {
+  async findManyPetsByCityOfOrganization(
+    searchParams: SearchParams,
+  ): Promise<PaginationResponse<Pet>> {
+    const organizationsId = await prisma.organization.findMany({
+      where: {
+        city: searchParams.city,
+        state: searchParams.state,
+      },
+      select: {
+        id: true,
+      },
+    })
+
+    if (searchParams.paginationDisabled) {
+      const pets = await prisma.pet.findMany({
+        where: {
+          organizationId: {
+            in: organizationsId.map((item) => item.id),
+          },
+          petAge: searchParams.age,
+          petEnergyLevel: searchParams.energyLevel,
+          petSize: searchParams.size,
+          petSpecies: {
+            contains: searchParams.species,
+          },
+          petIndependenceLevel: searchParams.independenceLevel,
+        },
+      })
+
+      return {
+        count: pets.length,
+        next: null,
+        previous: null,
+        page: 1,
+        totalPages: 1,
+        perPage: pets.length,
+        paginationDisabled: true,
+        results: pets,
+      }
+    }
+
+    const perPage = searchParams?.perPage ?? 10
+    const currentPage = searchParams?.page ?? 1
+
+    const count = await prisma.pet.count({
+      where: {
+        organizationId: {
+          in: organizationsId.map((item) => item.id),
+        },
+        petAge: searchParams.age,
+        petEnergyLevel: searchParams.energyLevel,
+        petSize: searchParams.size,
+        petSpecies: {
+          contains: searchParams.species,
+        },
+        petIndependenceLevel: searchParams.independenceLevel,
+      },
+    })
+
+    const totalPages = Math.ceil(count / perPage)
+
+    const petsWithPagination = await prisma.pet.findMany({
+      where: {
+        organizationId: {
+          in: organizationsId.map((item) => item.id),
+        },
+        petAge: searchParams.age,
+        petEnergyLevel: searchParams.energyLevel,
+        petSize: searchParams.size,
+        petSpecies: {
+          contains: searchParams.species,
+        },
+        petIndependenceLevel: searchParams.independenceLevel,
+      },
+      take: perPage,
+      skip: (currentPage - 1) * perPage,
+    })
+
+    const nextPage = totalPages === currentPage ? null : currentPage + 1
+    const previousPage = currentPage === 1 ? null : currentPage - 1
+
+    return {
+      count,
+      next: nextPage,
+      previous: previousPage,
+      page: currentPage,
+      perPage,
+      totalPages,
+      paginationDisabled: !!searchParams.paginationDisabled,
+      results: petsWithPagination,
+    }
+  }
+
   async findById(id: string): Promise<Organization | null> {
     const organization = await prisma.organization.findUnique({
       where: {
