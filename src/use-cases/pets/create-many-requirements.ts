c0f48e059338a7c39cadcf6ddea5ce97621798a9
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-10-02 14:37:26 -0300

    feat: creates use case to create many requirements

--- src/use-cases/pets/create-many-requirements.ts
+++ src/use-cases/pets/create-many-requirements.ts
@@ -1,13 +1,17 @@
 import type { PetRequirement } from '@prisma/client'
 
 import type { OrganizationsRepository } from '@/repositories/organizations-repository'
+import type { PetsRepository } from '@/repositories/pets-repository'
 import type { PetsRequirementRepository } from '@/repositories/pets-requirement-repository'
 
 import { ResourceNotFound } from '../_errors/resource-not-found'
 
 interface CreateManyRequirementsUseCaseRequest {
   organizationId: string
-  requirement: string[]
+  petId: string
+  requirement: Array<{
+    name: string
+  }>
 }
 
 interface CreateManyRequirementsUseCaseResponse {
@@ -17,22 +21,30 @@ interface CreateManyRequirementsUseCaseResponse {
 export class CreateManyRequirementsUseCase {
   constructor(
     private organizationsRepository: OrganizationsRepository,
+    private petsRepository: PetsRepository,
     private petsRequirementRepository: PetsRequirementRepository,
   ) {}
 
   async execute({
     organizationId,
+    petId,
     requirement,
   }: CreateManyRequirementsUseCaseRequest): Promise<CreateManyRequirementsUseCaseResponse> {
     const organization =
       await this.organizationsRepository.findById(organizationId)
 
-    if (organization) {
+    if (!organization) {
+      throw new ResourceNotFound()
+    }
+
+    const pet = await this.petsRepository.findById(petId)
+
+    if (!pet) {
       throw new ResourceNotFound()
     }
 
     const requirements = await this.petsRequirementRepository.createMany({
-      organizationId,
+      petId,
       requirement,
     })
 
